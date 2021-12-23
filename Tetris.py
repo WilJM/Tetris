e4f06f5118a62b989d67e21b3543d86662ed5e74
JM <jm4904@naver.com>
2021-12-23 17:31:35 +0900

    주석,  독스트링 추가 클래스, 함수 등의 기능 설명 추가

--- Tetris.py
+++ Tetris.py
@@ -1,17 +1,38 @@
+'''
+블록을 관리하기 위한 라이브러리
+
+    사각형  : 정사각형 1개
+    블록    : 사각형이 4개가 모여 형성되는 테트리스의 기본적인 도형 
+    ex )     
+            0000  <--- 블록,  0 <- 사각형
+'''
 from enum import Enum
 
 class Num(Enum):
+    '''
+    first, second, thrid, forth를 1, 2, 3, 4로 표현하기 위한 Enum 열거 클래스
+    '''
     first =0; second =1; third=2; forth =3
 
-enumerate
 class Shape:
+    '''
+    블록의 각 모양을 x좌표, y좌표로 표현하기 위한 클래스
+    사각형 4개를 합쳐 하나의 블록을 만들기 위함
+    
+    Attributes:
+        point       : 4개의 사각형의 [y,x] 좌표 (list)
+        rect_size   : point에서 1을 한칸으로 기준했기 때문에 원하는 사각형의 크기를 맞추기 위한 상수 (int)
+    Methods:
+        set_shape   : num을 통해 원하는 사각형의 x, y좌표를 지정할 수 있음
+        get_shape_x : num번째 사각형의 x좌표 return
+        get_shape_y : num번째 사각형의 y좌표 return
+    '''
     def __init__(self) -> None:
         self.point = [[0,0], [0,0], [0,0], [0,0]]
-        self.now = 0
-
+        self.rect_size = 10
     def set_shape(self, num, x, y) -> None:
-        self.point[num][1] = x*10
-        self.point[num][0] = y*10
+        self.point[num][1] = x * self.rect_size
+        self.point[num][0] = y * self.rect_size
 
     def get_shape_x(self, num):
         return self.point[num][1]
@@ -25,10 +46,60 @@ o_shape = [Shape(), Shape(), Shape(), Shape()]
 z_shape = [Shape(), Shape(), Shape(), Shape()]
 i_shape = [Shape(), Shape(), Shape(), Shape()]
 h_shape = [Shape(), Shape(), Shape(), Shape()]
-def init_shape():
-    #t_shape = [Shape(), Shape(), Shape(), Shape()]
 
-    
+'''
+한 블럭 당 4가지의 회전한 모양이 있기 때문에 shape 클래스 객체를 이용해 list형식으로 선언함
+t_shape경우 ㅗ ㅜ ㅏ ㅓ 네가지의 90도 회전한 모양 존재를 shape 클래스 객체로 표현
+'''
+
+def init_shape():
+    '''
+    t_shape, l_shape, o_shape, z_shape, i_shape, h_shape 블럭의 4가지 회전모양을 초기화한다.
+
+    (y,x)
+
+    (0,0) (0,1) (0,2) (0,3)
+    (1,0) (1,1) (1,2) (1,3)
+    (2,0) (2,2) (2,2) (2,3)
+    (3,0) (3,1) (3,2) (3,3)
+
+    <t_shape>
+    .0.. 0... 000. .0.. 1. (0,1) (1,0) (1,1) (1,2)
+    000. 00.. .0.. 00.. 2. (0,0) (1,0) (1,1) (2,0)
+    .... 0... .... .0.. 3. (0,0) (0,1) (0,2) (1,1)
+    .... .... .... .... 4. (0,1) (1,0) (1,1) (2,1)
+
+    <l_shape>
+    0... 0000 0... 0000 1. (0,0) (1,0) (2,0) (3,0)
+    0... .... 0... .... 2. (0,0) (0,1) (0,2) (0,3)
+    0... .... 0... .... 3. (0,0) (1,0) (2,0) (3,0)
+    0... .... 0... .... 4. (0,0) (0,1) (0,2) (0,3)
+
+    <o_shape>
+    00.. 00.. 00.. 00.. 1. (0,0) (0,1) (1,0) (1,1)
+    00.. 00.. 00.. 00.. 2. (0,0) (0,1) (1,0) (1,1)
+    .... .... .... .... 3. (0,0) (0,1) (1,0) (1,1)
+    .... .... .... .... 4. (0,0) (0,1) (1,0) (1,1)
+
+    <z_shape>
+    00.. .0.. 00.. .0.. 1. (0,0) (0,1) (1,1) (1,2)
+    .00. 00.. .00. 00.. 2. (0,1) (1,0) (1,1) (2,0)
+    .... 0... .... 0... 3. (0,0) (0,1) (1,1) (1,2)
+    .... .... .... .... 4. (0,1) (1,0) (1,1) (2,0)
+
+    <i_shape>
+    0... ..0. 00.. 000. 1. (0,0) (1,0) (2,0) (2,1)
+    0... 000. .0.. 0... 2. (1,0) (1,1) (1,2) (0,2) 
+    00.. .... .0.. .... 3. (0,0) (0,1) (1,0) (2,0) 
+    .... .... .... .... 4. (0,0) (0,1) (0,2) (1,0) 
+
+    <h_shape>
+    .00. 0... .00. 0... 1. (1,0) (1,1) (0,1) (0,2)
+    00.. 00.. 00.. 00.. 2. (0,0) (1,0) (1,1) (2,1)
+    .... .0.. .... .0.. 3. (1,0) (1,1) (0,1) (0,2)
+    .... .... .... .... 4. (0,0) (1,0) (1,1) (2,1)
+
+    '''
     t_shape[Num.first.value].set_shape(Num.first.value, 1, 0); t_shape[Num.first.value].set_shape(Num.second.value, 0,1); t_shape[Num.first.value].set_shape(Num.third.value, 1, 1); t_shape[Num.first.value].set_shape(Num.forth.value, 2, 1)
     t_shape[Num.second.value].set_shape(Num.first.value, 0, 0); t_shape[Num.second.value].set_shape(Num.second.value, 0, 1); t_shape[Num.second.value].set_shape(Num.third.value, 1, 1); t_shape[Num.second.value].set_shape(Num.forth.value, 0, 2) 
     t_shape[Num.third.value].set_shape(Num.first.value, 0, 0); t_shape[Num.third.value].set_shape(Num.second.value, 1, 0); t_shape[Num.third.value].set_shape(Num.third.value, 2, 0); t_shape[Num.third.value].set_shape(Num.forth.value, 1, 1) 
@@ -58,82 +129,19 @@ def init_shape():
     h_shape[Num.second.value].set_shape(Num.first.value, 0, 0); h_shape[Num.second.value].set_shape(Num.second.value, 0, 1); h_shape[Num.second.value].set_shape(Num.third.value, 1, 1); h_shape[Num.second.value].set_shape(Num.forth.value, 1, 2)
     h_shape[Num.third.value].set_shape(Num.first.value, 0, 1); h_shape[Num.third.value].set_shape(Num.second.value, 1, 1); h_shape[Num.third.value].set_shape(Num.third.value, 1, 0); h_shape[Num.third.value].set_shape(Num.forth.value, 2, 0)
     h_shape[Num.forth.value].set_shape(Num.first.value, 0, 0); h_shape[Num.forth.value].set_shape(Num.second.value, 0, 1); h_shape[Num.forth.value].set_shape(Num.third.value, 1, 1); h_shape[Num.forth.value].set_shape(Num.forth.value, 1, 2)
-    
-'''
-(y,x)
-
-(0,0) (0,1) (0,2) (0,3)
-(1,0) (1,1) (1,2) (1,3)
-(2,0) (2,2) (2,2) (2,3)
-(3,0) (3,1) (3,2) (3,3)
-
-<t_shape>
-.0.. 0... 000. .0.. 1. (0,1) (1,0) (1,1) (1,2)
-000. 00.. .0.. 00.. 2. (0,0) (1,0) (1,1) (2,0)
-.... 0... .... .0.. 3. (0,0) (0,1) (0,2) (1,1)
-.... .... .... .... 4. (0,1) (1,0) (1,1) (2,1)
-
-<l_shape>
-0... 0000 0... 0000 1. (0,0) (1,0) (2,0) (3,0)
-0... .... 0... .... 2. (0,0) (0,1) (0,2) (0,3)
-0... .... 0... .... 3. (0,0) (1,0) (2,0) (3,0)
-0... .... 0... .... 4. (0,0) (0,1) (0,2) (0,3)
-
-<o_shape>
-00.. 00.. 00.. 00.. 1. (0,0) (0,1) (1,0) (1,1)
-00.. 00.. 00.. 00.. 2. (0,0) (0,1) (1,0) (1,1)
-.... .... .... .... 3. (0,0) (0,1) (1,0) (1,1)
-.... .... .... .... 4. (0,0) (0,1) (1,0) (1,1)
-
-<z_shape>
-00.. .0.. 00.. .0.. 1. (0,0) (0,1) (1,1) (1,2)
-.00. 00.. .00. 00.. 2. (0,1) (1,0) (1,1) (2,0)
-.... 0... .... 0... 3. (0,0) (0,1) (1,1) (1,2)
-.... .... .... .... 4. (0,1) (1,0) (1,1) (2,0)
-
-<i_shape>
-0... ..0. 00.. 000. 1. (0,0) (1,0) (2,0) (2,1)
-0... 000. .0.. 0... 2. (1,0) (1,1) (1,2) (0,2) 
-00.. .... .0.. .... 3. (0,0) (0,1) (1,0) (2,0) 
-.... .... .... .... 4. (0,0) (0,1) (0,2) (1,0) 
-
-<h_shape>
-.00. 0... .00. 0... 1. (1,0) (1,1) (0,1) (0,2)
-00.. 00.. 00.. 00.. 2. (0,0) (1,0) (1,1) (2,1)
-.... .0.. .... .0.. 3. (1,0) (1,1) (0,1) (0,2)
-.... .... .... .... 4. (0,0) (1,0) (1,1) (2,1)
-
-'''
-    
-def left_click(num):
-    return num-1
-'''
-왼쪽 방향키 클릭시 shape 왼쪽 한칸 이동
-'''
-
-def right_click(num):
-    return num+1
-'''
-오른쪽 방향키 클릭시 shape 오른쪽 한칸 이동
-'''
-
-def down_click(num):
-    return num+1
-'''
-아랫 방향키 클릭시 shape 아랫쪽 한칸 이동
-'''
-
-def space_click(num):
-    return (num+1)%4
-'''
-스페이스 바 버튼 클릭시 시계방향으로 shape 90도 회전
-'''
 
 
 class Block:
+    '''
+    쌓여있는 사각형들을 저장하는 클래스
+    
+    Attributes:
+        blocks      : 쌓여있는 사각형들  (list)
+    Methods:
+        get_blocks  : num번째 쌓여있는 사각형의 list 리턴 
+        set_block   : 쌓이는 블럭이 생길 시 list에 추가
+    '''
     def __init__(self) -> None:
-        self.width = 200
-        self.height = 300
         self.blocks = [] 
 
     def get_blocks(self, num)-> list:
@@ -152,6 +160,18 @@ class Block:
         pass
 
 class Current:
+    '''
+    블록의 각 모양을 x좌표, y좌표로 표현하기 위한 클래스
+    사각형 4개를 합쳐 하나의 블록을 만들기 위함
+    
+    Attributes:
+        point       : 4개의 사각형의 [y,x] 좌표 (list)
+        rect_size   : point에서 1을 한칸으로 기준했기 때문에 원하는 사각형의 크기를 맞추기 위한 상수 (int)
+    Methods:
+        set_shape   : num을 통해 원하는 사각형의 x, y좌표를 지정할 수 있음
+        get_shape_x : num번째 사각형의 x좌표 return
+        get_shape_y : num번째 사각형의 y좌표 return
+    '''
     def __init__(self) -> None:
         self.shpae_num = 0
         self.pos = [[0,0], [0,0], [0,0], [0,0]]
@@ -169,6 +189,9 @@ class Current:
         return self.pos[num][0]
 
 def rand_block(num)->list:
+    '''
+    처음 나오는 블럭 형태를 랜덤으로 하기 위해 만든 함수
+    '''
     rand_shape = []
     for i in range(4):
         rand_shape += [t_shape[i].point] + [l_shape[i].point] + [o_shape[i].point] + [z_shape[i].point] + [i_shape[i].point] + [i_shape[i].point]
